f6c54cbae2c13c6ea4693b18f0d186a4635c83f1
Carlos Russo <carlos.russo@webdetails.pt>
2013-12-06 18:33:50 +0000

    initial commit

---
+++ static/custom/js/startupRuleEngine.js
@@ -0,0 +1,48 @@
+;(function(){
+
+
+    var checkboxAddIn = {
+        name: "checkbox",
+        label: "ActionCheckbox",
+        defaults:{
+            url:  '/pentaho/plugin/startupRuleEngine/api/setendpointlistener'
+        },
+
+        init: function(){
+
+        },
+
+        implementation: function(tgt, st, opt){
+            var options = $.extend(true,{},opt),
+                $tgt = $(tgt);
+
+            value = $(tgt).text();
+            $addIn = $('<input type="checkbox"/>').addClass('checkBoxAddIn');
+            $addIn.prop('checked', (value.toLowerCase() == "true"));
+            $addIn.click(function(){
+                var eventName = st.rawData.metadata[st.colIdx]['colName'],
+                    endpointName = st.rawData.resultset[st.rowIdx][0],
+                    enableEvent = $(tgt).find(':checkbox').prop('checked');
+
+                Dashboards.log("Setting " + eventName +  " of "+endpointName+ ' to ' + enableEvent, 'debug') ;
+                //Dashboards.log(JSON.stringify(st));
+                // Call the endpoint
+                var url = options.url;
+                url += '?paramfilename=' +  st.rawData.resultset[st.rowIdx][3];
+                url += '&paramevent=' +  eventName;
+                url += '&paramvalue=' + enableEvent;
+                $.ajax(url, {
+                    dataType: 'json',
+                    mimeType: 'application/json; charset utf-8',
+                    type: 'POST',
+                    error: function(){
+                        $(tgt).find(':checkbox').prop('checked', !enableEvent);
+                    }
+                });
+            });
+            $(tgt).empty().append($addIn);
+        }
+
+    };
+    Dashboards.registerAddIn("Table", "colType", new AddIn(checkboxAddIn));
+})();
